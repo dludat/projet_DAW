317a017697b48281dd1dbf9541ecdb4ff4d1ee00
David <dludat@students.uni-mainz.de>
2026-03-04 18:08:58 +0100

    structure fondamentale

---
+++ inscription.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <link href="inscription.css" rel="stylesheet"></link>
+    </head>
+    <body>
+        <h1>Inscription pour Helpdesk</h1>
+        <form action ="inscription.php" method="post">
+            Nom d'utilisateur: <input type="text" name="nom"><br>
+            Adresse de mail universitaire <input type="text" name="mail"><br>
+            Mot de passe: <input type="password" name="motpasse"><br>
+            Repeter mot de passe: <input type="password" name="motpasse"><br>
+            <button nom="valider">Valider</button>
+        </form>
+
+    </body>
+</html>
+<?php
+echo "bonjour";
+
+//Formulaire d'inscription et vérification
+?>
--- login.php
+++ login.php
@@ -1,2 +1,27 @@
-$input = $_POST("nom");
-print($input)
+<?php
+$erreurs = [];
+
+$nom = $_POST["nom"] ?? "";
+echo $nom . "<br>";
+if ($nom === "") {
+    $erreurs[] = "Nom d'utilisateur obligatoire<br>";
+}
+$mot_de_passe = $_POST["mdp"] ?? "";
+if ($mot_de_passe === "") {
+    $erreurs[] = "Mot de passe obligatoire<br>";
+}
+
+if (!empty($erreurs)) {
+    foreach ($erreurs as $e) {
+        echo $e;
+    }
+} else { //vérifier mot de passe et ajouter role (etudiant/tuteur)
+    $_SESSION["role"] = "etudiant"; 
+}
+?>
+<html>
+    <body>
+        <button type="button" onclick="window.location.href='./index.html' ">back</button>
+        <button type="button" onclick="window.location.href=' ./etudiant.html'">site d'apercu</button>
+    </body>
+</html>
