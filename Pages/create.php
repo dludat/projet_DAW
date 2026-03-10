e54141ea5ce4c3ba79378e10bbc2b16426f84fc1
David <dludat@students.uni-mainz.de>
2026-03-10 15:18:00 +0100

    formulaire create incl. validation

--- Pages/create.php
+++ Pages/create.php
@@ -1,6 +1,32 @@
 <?php
-//télécharger la liste des cours de la BDD
-echo "<datalist id='cours_options>"
+session_start();
+//afficher les erreurs
+if (isset($_SESSION["error"])) {
+    echo "<p id='erreur' style='color:red'> " . $_SESSION["error"] . "</p>";
+    unset($_SESSION["error"]); 
+}
+
+//Télécharger les subjects et tuteurs possibles
+$cours; //ici inserer les cours possibles avec noms et id
+$tuteurs; //ici inserer les tuteurs possibles avec noms et id (tutor_subjects table)
+$cours_list;
+$tuteurs_list;
+
+//écrire options pour <select> de cours
+//foreach ($cours as $c) { //ajouter tous les options
+//    $cours_list .= "<option value='" . htmlspecialchars($c->id) . "'>" . htmlspecialchars($c->name) . "</option>";
+//}
+//$list .= "</datalist>";
+
+//écrire options pour <select> de tuteurs
+//foreach ($tuteurs as $t) { //ajouter tous les options
+//    $tuteurs_list .= "<option value='" . htmlspecialchars($t->id) . "'>". htmlspecialchars($t->name) . "</option>";
+//}
+
+//notes pour sauvegarder
+//Cours: <input name="cours" type="select" list="cours" placeholder="Rechercher des cours" required>
+//Tuteur: <input name="tuteur" type="select" list="cours_options" required><br>
+//$list = "<datalist id='cours_options' form='nv_ticket'>";
 ?>
 
 <html>
@@ -10,11 +36,16 @@ echo "<datalist id='cours_options>"
     </head>
     <body>
         <h1>Creer nouveau ticket:</h1>
-        <form action="../Actions/create_ticket_action.php" method="get">
+        <form id="nv_ticket" action="../Actions/create_ticket_action.php" method="post">
             Titre: <input name="titre" type="text" required><br>
-            Cours: <input name="cours" type="search" list="" placeholder="Rechercher des cours" required>
-            <button type="button" name="recherche_cours">Rechercher</button><br>
-            Tuteur: <input name="tuteur" type="search" required list="cours_options"><br>
+            Cours:<select name="cours" required>
+                <option value="1">$c.name</option>
+                <?php //echo $cours_list; //ajouter tous les options?>
+            </select><br>
+            Tuteurs:<select name="tuteur" required>
+                <option value="2">$c.name</option>
+                <?php //echo $tuteurs_list; //ajouter tous les options?>
+            </select><br>
 
             <fieldset>
                 <legend>Sélectionnez la priorité du ticket</legend>
@@ -35,16 +66,12 @@ echo "<datalist id='cours_options>"
                 <input type="radio" name="categorie" id="tp" value="3">
                 <label for="tp">TP</label><br>
             </fieldset><br>
-            <label>Déscrivez votre problème en détail</label>
+            <label for="description">Décrivez votre problème en détail</label>
             <textarea type="text" name="description" cols="30" rows="10">
             </textarea>
             <br>
+            <button type="submit" id="valider">Valider</button>
         </form>
         <button type="button" id="retour" onclick="window.location.href='./etudiant'">Retour à l'aperçu</button>
     </body>
 </html>
-
-<?php
-
-//Function de recherche pour les matières
-?> 
