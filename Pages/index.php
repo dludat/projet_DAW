aed940feb6ee0c8cac37203ed928941512a674c7
Charango4554 <ryanbenhadji@protonmail.com>
2026-03-22 14:00:40 +0100

    Simplification Index

--- Pages/index.php
+++ Pages/index.php
@@ -1,24 +1,27 @@
 <?php
-//afficher les erreurs
-if (isset($_SESSION["error"])) {
-    echo "<p id='erreur' style='color:red'> " . $_SESSION["error"] . "</p>";
-    unset($_SESSION["error"]); 
-}
+session_start();
 
+// afficher les erreurs
+if (isset($_SESSION["error"])) {
+    echo "<p id='erreur' style='color:red'>" . $_SESSION["error"] . "</p>";
+    unset($_SESSION["error"]);
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <title>Bienvenue sur Helpdesk!</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="/css/index.css">
 </head>
 <body>
     <h1>Bienvenue sur Helpdesk!</h1>
-    <h3>Login</h3>
-    <form id = "login" action="Pages/login.php" method="post">
-        Nom: <input type="text" name="nom"><br>
-        Mot de passe: <input type="password" name="mdp"><br>
-        <button id="valider">Se connecter</button>
-        <button id="inscription" type="button" onclick="window.location.href='Pages/inscription.php'">S'inscrire</button>
-    </form>
+    <h3>Choisissez une action</h3>
+
+    <button id="connexion" type="button" onclick="window.location.href='/Pages/login.php'">
+        Se connecter
+    </button>
+    <button id="inscription" type="button" onclick="window.location.href='/Pages/inscription.php'">
+        S'inscrire
+    </button>
+    </body>
 </html>
