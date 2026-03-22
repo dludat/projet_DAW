649aeb52b1ae42af582196642752cb553a4d7465
David <dludat@students.uni-mainz.de>
2026-03-22 11:41:35 +0100

    option de modifier les tickets pour tuteurs

---
+++ Actions/update_ticket_action.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    //Récupérer les données
+    $erreurs = array();
+    $ticket_id = intval($_POST["ticket_id"]) ?? 0;
+    $priorite = intval($_POST["priorite"]) ?? 0;
+    $statut = intval($_POST["statut"]) ?? 0;
+    $categorie = intval($_POST["categorie"]) ?? 0;
+
+    if ($ticket_id === 0) {
+        $erreurs[] = "Problème interne. Essaye de nouveau";
+    }if ($priorite === 0) {
+        $erreurs[] = "Veuillez définir une priorité";
+    }
+    if ($statut === 0) {
+        $erreurs[] = "Veuillez définir un statut";
+    }
+    if ($categorie === 0) { 
+        $erreurs[] = "Veuillez définir une catégorie";
+    }
+    if (empty($erreurs)) {
+        //Modifier la base de données
+
+        //afficher le succes
+        $_SERVER["succes"] = "La modification a été un succès.";
+        header("Location: ../Pages/tickets.php");
+        exit();
+    } else {
+        //afficher les erreurs
+        $_SERVER["error"] = "";
+        foreach ($erreurs as $e) { 
+            $_SERVER["error"] .= $e . "\n";
+        }
+        header("Location: ../Pages/tickets.php");
+        exit();
+    }
+}
